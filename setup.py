008ba27ec41c951b85d85a1b882fc5746daea3b8
Martin Patz <mailto@martin-patz.de>
2019-07-30 23:06:17 +0200

    adds a setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="scripts",
+    version="0.1",
+    packages=[],
+    scripts=['tree-file-count'],
+    install_requires=['argparse', 'tree-format'],
+    author="Martin Patz",
+    author_email="mailto@martin-patz.de",
+    description="A loose collection of scripts.",
+    license="GNU GENERAL PUBLIC LICENSE",
+    license_file="LICENSE",
+)
